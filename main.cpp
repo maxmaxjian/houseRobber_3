9817caa3242a53092309fa36f20e99f72a6cc0ff
Jian Wei <jianwei@jianwei-deb8-64>
2016-09-29 17:58:49 -0400

    second version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <memory>
-// #include <functional>
-// #include <unordered_set>
-// #include <vector>
-// #include <queue>
+// solution 1
 #include <iostream>
+#include <memory>
+#include <functional>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+#include <algorithm>
 
 class TreeNode {
   public:
@@ -21,98 +23,154 @@ class TreeNode {
             result ^= right->hash();
         return result;
     }
+
+    void print() {
+        if (left != nullptr || right != nullptr) {
+            std::cout << "val = " << val;
+            if (left != nullptr)
+                std::cout << "\t left -> " << left->val << std::endl;
+            else
+                std::cout << "\t left -> NULL" << std::endl;
+            if (right != nullptr)
+                std::cout << "\t right -> " << right->val << std::endl;
+            else
+                std::cout << "\t right -> NULL" << std::endl;
+        }
+        else
+            std::cout << "val = " << val << std::endl;
+    }
 };
 
 using pNode = std::shared_ptr<TreeNode>;
 
-// class TreeNodeHasher {
-//   public:
-//     size_t operator()(const pNode & node) {
-//         return node->hash();
-//     }
-// };
+class TreeNodeHasher {
+  public:
+    size_t operator()(const pNode & node) {
+        return node->hash();
+    }
+};
 
-// const pNode & getParent(const pNode & root, const pNode & curr) {
-//     std::queue<pNode> qu;
-//     qu.push(root);
-//     while (!qu.empty()) {
-//         auto & node = qu.front();
-//         qu.pop();
-//         if (node == curr)
-//             return nullptr;
-//         if (node->left == curr || node->right == curr)
-//             return node;
-//         if (node->left != nullptr)
-//             qu.push(node->left);
-//         if (node->right != nullptr)
-//             qu.push(node->right);
-//     }
-// }
-//
-// class solution {
-//   public:
-//     int rob(const pNode & root) {
-//         std::unordered_set<pNode> visited;
-//         std::vector<std::unordered_set<pNode>> paths = robFrom(root, root, visited);
-//         std::vector<int> cands;
-//         for (auto & path : paths) {
-//             int sum = 0;
-//             for (auto it = path.begin(); it != path.end(); ++it)
-//                 sum += (*it)->val;
-//             cands.push_back(sum);
-//         }
-
-//         return *std::max_element(cands.begin(), cands.end());
-//     }
-
-//   private:
-//     std::vector<std::unordered_set<pNode>> robFrom(const pNode & root, const pNode & curr, const std::unordered_set<pNode> & visited) {
-//         std::vector<std::unordered_set<pNode>> result;
-//         if (curr == nullptr) {
-//             result.push_back(visited);
-//         }
-//         else {
-//             pNode parent = getParent(root, curr);
-//             if (parent == nullptr) {
-//                 std::unordered_set<pNode> tmp1;
-//                 tmp1.insert(curr);
-//                 auto left = 
-                
-//                 std::unordered_set<pNode> tmp2;
-//                 result.push_back(tmp1);
-//                 result.push_back(tmp2);
-//             }
-//             else {
-//                 if (visited.find(parent) != visited.end()) {
-                    
-//                 }
-//             }
-//         }
-        
-//     }
-// };
+const pNode & getParent(const pNode & root, const pNode & curr) {
+    std::queue<pNode> qu;
+    qu.push(root);
+    while (!qu.empty()) {
+        auto & node = qu.front();
+        qu.pop();
+        if (node == curr)
+            return nullptr;
+        if (node->left == curr || node->right == curr)
+            return node;
+        if (node->left != nullptr)
+            qu.push(node->left);
+        if (node->right != nullptr)
+            qu.push(node->right);
+    }
+}
 
 class solution {
   public:
     int rob(const pNode & root) {
-        int res1 = robFrom(root, true), res2 = robFrom(root, false);
-        return std::max(res1, res2);
+        std::unordered_set<pNode> visited;
+        std::vector<std::unordered_set<pNode>> paths = robFrom(root, root, visited);
+
+        std::cout << "All possible paths are:" << std::endl;
+        for (auto path : paths) {
+            if (!path.empty()) {
+                for (auto it = path.begin(); it != path.end(); ++it)
+                    std::cout << (*it)->val << " ";
+                std::cout << std::endl;
+            }
+        }
+        
+        std::vector<int> cands;
+        for (auto & path : paths) {
+            int sum = 0;
+            for (auto it = path.begin(); it != path.end(); ++it)
+                sum += (*it)->val;
+            cands.push_back(sum);
+        }
+
+        return *std::max_element(cands.begin(), cands.end());
     }
 
   private:
-    int robFrom(const pNode & root, bool canRobRoot) {
-        if (root == nullptr)
-            return 0;
+    std::vector<std::unordered_set<pNode>> robFrom(const pNode & root, const pNode & curr, std::unordered_set<pNode> & visited) {
+        std::vector<std::unordered_set<pNode>> result;
+        if (curr == nullptr) {
+            result.push_back(visited);
+        }
         else {
-            if (canRobRoot) {
-                return root->val + robFrom(root->left, false) + robFrom(root->right, false);
+            pNode parent = getParent(root, curr);
+            // if the parent of curr node is not visited
+            if (visited.find(parent) == visited.end()) {
+                // we can next visit curr
+                auto cpy = visited;
+                cpy.insert(curr);                
+                auto left = robFrom(root, curr->left, cpy);
+                for (auto elem : left) {
+                    auto right = robFrom(root, curr->right, elem);
+                    for (auto elm : right)
+                        result.push_back(elm);
+                }
+
+                // or we can next visit curr's left and right children
+                auto cpy2 = visited;                
+                auto left2 = robFrom(root, curr->left, cpy2);
+                for (auto elem : left2) {
+                    auto right2 = robFrom(root, curr->right, elem);
+                    for (auto elm : right2)
+                        result.push_back(elm);
+                }
             }
             else {
-                return robFrom(root->left, true)+robFrom(root->right, true);
+                auto cpy2 = visited;                
+                auto left = robFrom(root, curr->left, cpy2);
+                for (auto elem : left) {
+                    auto right = robFrom(root, curr->right, elem);
+                    for (auto elm : right)
+                        result.push_back(elm);
+                }
             }
         }
+        return result;
     }
 };
+
+
+
+// solution 2
+// #include <iostream>
+// #include <memory>
+//
+// class TreeNode {
+//   public:
+//     int val;
+//     std::shared_ptr<TreeNode> left, right;
+
+//     TreeNode(int xval) : val(xval), left(nullptr), right(nullptr){}
+// };
+
+// class solution {
+//   public:
+//     int rob(const pNode & root) {
+//         int res1 = robFrom(root, true), res2 = robFrom(root, false);
+//         return std::max(res1, res2);
+//     }
+
+//   private:
+//     int robFrom(const pNode & root, bool canRobRoot) {
+//         if (root == nullptr)
+//             return 0;
+//         else {
+//             if (canRobRoot) {
+//                 return root->val + robFrom(root->left, false) + robFrom(root->right, false);
+//             }
+//             else {
+//                 return robFrom(root->left, true)+robFrom(root->right, true);
+//             }
+//         }
+//     }
+// };
 
 int main() {
     // pNode root = std::make_shared<TreeNode>(3);
@@ -121,12 +179,12 @@ int main() {
     // root->left->right = std::make_shared<TreeNode>(3);
     // root->right->right = std::make_shared<TreeNode>(1);
 
-    pNode root = std::make_shared<TreeNode>(3);
-    root->left = std::make_shared<TreeNode>(4);
-    root->right = std::make_shared<TreeNode>(5);
-    root->left->left = std::make_shared<TreeNode>(1);
+    pNode root = std::make_shared<TreeNode>(1);
+    root->left = std::make_shared<TreeNode>(5);
+    root->right = std::make_shared<TreeNode>(6);
+    root->left->left = std::make_shared<TreeNode>(2);
     root->left->right = std::make_shared<TreeNode>(3);
-    root->right->right = std::make_shared<TreeNode>(1);
+    root->right->right = std::make_shared<TreeNode>(4);
 
     solution soln;
     int result = soln.rob(root);
